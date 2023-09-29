a5d29e973a99ab40a073402297ad0d17062e0ff6
alice duong <thachthao.8401@gmail.com>
2023-09-29 02:07:18 -0400

    challenge0_1

--- app/src/main/java/com/driuft/androidkotlindebugging/MainActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun launchHelloWorldActivity() {
-        val i = Intent(404 as Context, HelloWorldActivity::class.java)
+        val i = Intent(this, HelloWorldActivity::class.java)
         startActivity(i)
     }
 
